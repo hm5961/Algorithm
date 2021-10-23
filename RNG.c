e1693ff76b5d785576f0ce5078a3d7a005568db4
hm5961 <71277818+hm5961@users.noreply.github.com>
2021-10-23 19:08:25 +0900

    Add files via upload

---
+++ RNG.c
@@ -0,0 +1,38 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <time.h>
+//#include <stdlib.h>
+//#define MAX_COUNT	50000
+//int main()
+//{
+//	FILE* p_file = fopen("50000.txt", "wt");
+//	int data[MAX_COUNT], i, sub_i;
+//	srand((unsigned int)time(NULL));
+//
+//	if (p_file != NULL)
+//	{
+//		for (int i = 0; i < MAX_COUNT; i++)
+//		{
+//			data[i] = rand() % MAX_COUNT + 1;
+//			for (sub_i = 0; sub_i < i; sub_i++)
+//			{
+//				if (data[i] == data[sub_i])
+//				{
+//					i--;
+//					break;
+//				}
+//
+//			}
+//			printf("%d\n", i);
+//		}	
+//
+//		for (i = 0; i < MAX_COUNT; i++)
+//		{
+//			printf("1");
+//			fprintf(p_file, "%d ", data[i]);
+//
+//		}
+//		fprintf(p_file, "%d ", -1);
+//	}
+//	return 0;
+//}
